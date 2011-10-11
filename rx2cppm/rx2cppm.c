75bb0c305ab647c3ec6bcc96839d9facc94b0c53
Sebastian Herp <sebbilein@gmail.com>
2011-10-11 02:15:40 +0200

    Testprogram for picdem 2 plus demo board Ports are strange: RA0/RA1 are connected to RA2/RA3 RA2/RA3 are connected to RB0/RB1 RB0/RB1 are connected to RB2/RB3 RB2/RB3 are connected to RA1/RA0

--- rx2cppm/rx2cppm.c
+++ rx2cppm/rx2cppm.c
@@ -1,46 +1,37 @@
 
-#include <p18f1230.h>
+#include <p18cxxx.h>
 #include "rx2cppm.h"
-
-/*
-
-	_CONFIG1( JTAGEN_OFF &		//JTAG OFF
-	 GCP_OFF & 					//CODE PROTECT OFF
-	 GWRP_OFF & 				//WRITE PROTECT OFF
-	 BKBUG_OFF &				//BACKGROUND DEBUGGER OFF
-	 COE_OFF & 					//CLIP ON EMULATION (reserved, always 1 on PIC24F) OFF
-	 ICS_PGx2 & 				//USING PGx2 pins
-	 FWDTEN_OFF & 				//WATCHDOG OFF
-	 WINDIS_OFF &				//WINDOWED WDT OFF
-	 FWPSA_PR128 &				//WATCHDOG PRESCALER
-	 WDTPS_PS32768)				//WATCHDOG POSTSCALER
-
-	_CONFIG2( IESO_ON &			//TWO SPEED STARTUP ENABLED
-	 //SOSCSEL_LPSOSC &			//ENABLE SECONDARY LOW POWER OSCILATOR (deactivated: causes weird startup
-	 							// behaviour with newer chip revisions)
-	 WUTSEL_FST &				//FAST WAKE-UP (unimplemented)
-	 FNOSC_FRC & 				//USING INTERNAL FAST RC OSCILATOR
-	 FCKSM_CSDCMD &	//CLOCK MONITOR / FAILSAFE DISABLED
-	 OSCIOFNC_ON & 				//OSCI PINS CAN BE USED AS NORMAL IO PORTS
-	 IOL1WAY_OFF &				//UNLIMITED WRITES TO RP REGISTERS
-	 I2C1SEL_PRI &				//USE PRIMARY I2C1 pins
-	 POSCMOD_NONE)	 			//NO PRIMARY OSCILATOR
-*/
-
+#include "delays.h"
 
 
 void setup(void) {
+	// Initializing ports
+    PORTA = 0;
+    PORTB = 0;
+
+    // Set RA4 as input and RB3-RB0 as output
+    TRISA &= 0x00;
+    TRISB &= 0x00;
 
 }
 
-void loop(void) {
+int counter = 0;
 
+void loop(void) {
+	
 }
 
 
 void main(void)
 {
 	setup();
-	while(1) loop();	
+	while(1) {
+		LATB = 3; 
+		LATA = 0;
+	    Delay1KTCYx(100); 
+	    LATB = 0;
+		LATA = 12;
+	    Delay1KTCYx(100); 
+	}	
 }
 
